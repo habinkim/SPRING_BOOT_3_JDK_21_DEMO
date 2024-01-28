bed4d9342e8795c0822593faacaeb1f67216749b
habin <danny9643@naver.com>
2024-01-28 12:44:43 +0900

    first commit

---
+++ src/test/java/com/habin/demo/DemoApplicationConfig.java
@@ -0,0 +1,37 @@
+package com.habin.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.containers.MariaDBContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class DemoApplicationConfig {
+
+    @Bean
+    @ServiceConnection
+    KafkaContainer kafkaContainer() {
+        return new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:latest"));
+    }
+
+    @Bean
+    @ServiceConnection
+    MariaDBContainer<?> mariaDbContainer() {
+        return new MariaDBContainer<>(DockerImageName.parse("mariadb:latest"));
+    }
+
+    @Bean
+    @ServiceConnection(name = "redis")
+    GenericContainer<?> redisContainer() {
+        return new GenericContainer<>(DockerImageName.parse("redis:latest")).withExposedPorts(6379);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.from(DemoApplicationConfig::main).with(DemoApplicationConfig.class).run(args);
+    }
+
+}
