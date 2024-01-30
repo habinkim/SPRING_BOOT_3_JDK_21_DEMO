4a7ca3599cd13bd62ce74d8eb42162f43c832726
habin <hbkim@cyrexpay.com>
2024-01-30 10:10:07 +0900

    docker-compose 수정

--- src/main/java/com/habin/demo/account/adapter/input/web/RefreshController.java
+++ src/main/java/com/habin/demo/account/adapter/input/web/RefreshController.java
@@ -1,5 +1,6 @@
 package com.habin.demo.account.adapter.input.web;
 
+import com.habin.demo.account.application.port.input.usecase.RefreshUseCase;
 import com.habin.demo.common.hexagon.WebAdapter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,4 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class RefreshController {
+
+    private final RefreshUseCase refreshUseCase;
+
 }
