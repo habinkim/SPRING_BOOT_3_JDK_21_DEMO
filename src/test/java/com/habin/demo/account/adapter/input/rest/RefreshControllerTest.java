c09e35371c76062fae476a18f502c8ef1005e24e
habin <hbkim@cyrexpay.com>
2024-02-02 17:58:38 +0900

    Refresh API 테스트 추가

--- src/test/java/com/habin/demo/account/adapter/input/rest/RefreshControllerTest.java
+++ src/test/java/com/habin/demo/account/adapter/input/rest/RefreshControllerTest.java
@@ -1,7 +1,32 @@
 package com.habin.demo.account.adapter.input.rest;
 
+import com.habin.demo.base.ControllerBaseTest;
+import com.habin.demo.dummy.DummyService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.security.NoSuchAlgorithmException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class RefreshControllerTest {
+class RefreshControllerTest extends ControllerBaseTest {
+
+    @Autowired
+    private DummyService dummyService;
+
+    @Transactional
+    @Test
+    @Order(1)
+    @DisplayName("Access Token 재발급, 성공")
+    void refreshSuccess() throws NoSuchAlgorithmException {
+        dummyService.initAndLogin();
+        String refreshToken = dummyService.getDefaultRefreshToken();
+
+
+
+    }
 
 }
